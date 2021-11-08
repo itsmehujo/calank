f9fa908d6833e858a8017984da4c86cdb02b5584
Guillaume <guillaume.gery13@gmail.com>
2021-11-08 17:22:49 +0100

    requete de creation base de donnee

---
+++ requete.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE CleanMyCalanque;
+
+CREATE USER 'utilisateur'@'localhost' IDENTIFIED BY 'calanque';
+
+GRANT ALL CleanMyCalanque.* TO 'utilisateur'@'localhost' IDENTIFIED BY 'calanque';
+
+CREATE TABLE sorties(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    lieu VARCHAR(150),
+    date DATE,
+    cleaner INT,
+    nb_sac INT,
+    kilos INT,
+    verre FLOAT,
+    plastique FLOAT,
+    carton FLOAT,
+    canette FLOAT,
+    tout_venant FLOAT,
+    encombrant FLOAT,
+    megots FLOAT,
+    bouchon FLOAT,
+    proto FLOAT
+
+);
+
