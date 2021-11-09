f15395475359a66137d9060af9a92ba84f59487a
mathieu-duverne <mathieu.duverne@laplateforme.io>
2021-11-09 23:59:24 +0100

    exemple_pdfchart is okay except tableau

--- exemple_pdfchart/index.php
+++ exemple_pdfchart/index.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>exemple pdf to chart</title>
+    <script src="https://kendo.cdn.telerik.com/2021.3.914/js/jquery.min.js"></script>
+    <script src="https://kendo.cdn.telerik.com/2021.3.914/js/kendo.all.min.js"></script>
 </head>
 <body>
 <button class='export-pdf k-button'>Save as PDF</button>
@@ -17,63 +19,26 @@
       </article>
       <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
       <script src="../dataViz/index.js"></script>
-      <script> $(".export-pdf").click(function () {
+      <script>
+          $(".export-pdf").click(function() {
             $("#myChart").getKendoChart().saveAsPDF();
         });
-
-        var series = [{
-            name: "cleaners",
-            data: [75, 203, 205, 908, 975],
-        }];
       function createChart() {
         $("#myChart").kendoChart({
                 
-                title: {
-                    text: "export title"
-                },
+    title: {
+         text: "Kendo Chart Example"
+    },
+    series: [
+         { name: "Example Series", data: [200, 450, 300, 125] }
+    ],
+    categoryAxis:{
+         categories: [ 2000, 2001, 2002, 2003 ]
+    }
+});
+      }
 
-                legend: {
-                    position: "bottom"
-                },
-
-                seriesDefaults: {
-                    type: "column",
-                    stack: true}
-                },
-                categoryAxis: {
-                    categories: [2017, 2018, 2019, 2020, 2021],
-                    majorGridLines: {
-                        visible: false
-                    }
-                },
-
-                tooltip: {
-                    visible: true,
-                    format: 0
-                
-            });
-          }
-
-          $(document).ready(function () {
-            createChart();
-            $(document).bind("kendo:skinChange", createChart);
-            $(".options").bind("change", refresh);
-        });
-
-        function refresh() {
-            var chart = $("#myChart").data("kendoChart"),
-                type = $("input[name=seriesType]:checked").val(),
-                stack = $("#stack").prop("checked");
-
-            for (var i = 0, length = series.length; i < length; i++) {
-                series[i].stack = stack;
-                series[i].type = type;
-            };
-
-            chart.setOptions({
-                series: series
-            });
-        }
+      $(document).ready(createChart);
         
         </script>
 </body>
